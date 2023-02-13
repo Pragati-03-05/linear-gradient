b70ce8856e99dac8da7c751023870b8d496b0229
Pragati-03-05 <122167332+Pragati-03-05@users.noreply.github.com>
2023-02-13 19:48:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# linear-gradient
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-22gyew)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="outer">
+      <div id="inner">Pragati</div>
+    </div>
+  );
+}
